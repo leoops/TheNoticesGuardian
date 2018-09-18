9686624a656f6f656eebcea3cb74e82ba7ace28f
Leonardo Pereira <leonardopereira519@gmail.com>
2018-09-18 09:58:46 -0300

    Initial Commit

---
+++ TheNoticesGuardian/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheNoticesGuardian
+//
+//  Created by Stefanini on 18/09/18.
+//  Copyright © 2018 Stefanini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
